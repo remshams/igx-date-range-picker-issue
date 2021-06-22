016dc42abb4511a404faa8889212b35b3a6fff7e
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-06-22 14:59:34 +0200

    * adds DateRangeComponent example showcasing the disabled issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
+import { DateRange } from 'igniteui-angular';
+import { interval, Observable } from 'rxjs';
+import { scan, startWith, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  /**
+   * When changing the "Default" the initial disabled value is reflected.
+   */
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'igx-date-range-picker-issue';
+  range: DateRange = {
+    start: new Date(),
+    end: new Date(new Date().getTime() + 86400000),
+  };
+
+  disabled$: Observable<boolean>;
+
+  constructor(ref: ChangeDetectorRef) {
+    // requestAnimationFrame(() => {
+    //   ref.detectChanges();
+    // });
+    this.disabled$ = this.setupDisabled();
+  }
+
+  setupDisabled(): Observable<boolean> {
+    return interval(4000).pipe(
+      scan((disabled) => !disabled, true as boolean),
+      tap((disabled) => console.log(disabled)),
+      startWith(true)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IgxDateRangePickerModule } from 'igniteui-angular';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     IgxDateRangePickerModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
